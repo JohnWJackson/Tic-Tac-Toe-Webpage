80bcbeed6ff74afa420c9cc5b2fdfa9224008ca8
Johnwjackson@yahoo.comgit config --global user.name JohnJgit config --global user.email Johnwjackson@yahoo.com <Johnwjackson@yahoo.com>
2022-01-16 00:03:57 -0500

    Functional tic-tac-toe game, and reset button

--- game.js
+++ game.js
@@ -1,12 +1,19 @@
 const Player = (side) => {
   let turn = false;
   let playerSide = side;
+  let winner = false;
   const getSide = () => playerSide;
   const isTurn = () => turn;
   const startTurn = () => { turn = true };
   const finishedTurn = () => { turn = false };
   const resetPlayer = () => { turn = false; playerSide = '';}
-  return {getSide, isTurn, startTurn, finishedTurn, resetPlayer}
+  const getWinner = () => winner;
+  const declareWinner = () => { winner = true; }
+  const declareTie = () => { winner = null; }
+  return { getSide, isTurn, 
+            startTurn, finishedTurn, 
+            resetPlayer, getWinner, 
+            declareWinner, declareTie }
 };
 
 const gameBoard = (() => {
@@ -15,74 +22,25 @@ const gameBoard = (() => {
                 'B1': '', 'B2': '', 'B3': '', 
                 'C1': '', 'C2': '', 'C3': ''  }
 
-  // Clear board and initialize
-  const newBoard = () => {
-    const squares = document.querySelectorAll('.square');
-    squares.forEach(sq => {
-      sq.textContent = '';
-    });
-  }
-
-  // Stores the board state after a turn
-  const checkIfWinner = (square) => {
-        let winConditions = { 'row_1' : 0, 'row_2' : 0, 'row_3' : 0,
-                          'column_1' : 0, 'column_2': 0, 'column_3': 0,
-                          'diaganol_1' : 0, 'diaganol_2' : 0 }
-    
-    for (i = 0; i < Object.keys(board).length; i++) {
-      if (Object.values(board)[i] == Player.getSide()) {
-        switch (Object.keys(board)[i]) {
-          case 'A1': winConditions['row_1']++;
-                     winConditions['column_1']++;
-                     winConditions['diaganol_1']++;
-                     
-                     break;
-          case 'A2': winConditions['row_1']++;
-                     winConditions['column_2']++;
-                     break;
-          case 'A3': winConditions['row_1']++;
-                     winConditions['column_3']++;
-                     winConditions['diaganol_2']++;
-                     break;
-          case 'B1': winConditions['row_2']++;
-                     winConditions['column_1']++;
-                     break;
-          case 'B2': winConditions['row_2']++;
-                     winConditions['column_2']++;
-                     winConditions['diaganol_1']++;
-                     winConditions['diaganol_2']++;
-                     break;
-          case 'B3': winConditions['row_2']++;
-                     winConditions['column_3']++;
-                     break;
-          case 'C1': winConditions['row_3']++;
-                     winConditions['column_1']++;
-                     winConditions['diaganol_2']++;
-                     break;
-          case 'C2': winConditions['row_3']++;
-                     winConditions['column_2']++;
-                     break;
-          case 'C3': winConditions['row_3']++;
-                     winConditions['column_3']++;
-                     winConditions['diaganol_1']++;
-                     break;
-        }
-      }
-    }
-  }
-
   // Event listeners and perform actions when board clicked
-  const boardListen = (Player) => {
+  const boardListen = (Player1, Player2) => {
     //Event listeners on game squares
     const squares = document.querySelectorAll('.square');
     squares.forEach(sq => {
       sq.addEventListener('click', function() {
+        const Player = Player1.isTurn() ? Player1 : Player2;
+        console.log(Player);
         if (isValid(sq)) {
           placeMove(sq, Player);
-          if (isWinner(Player)) {
-            console.log("WINNER");
-
+          if (isTie()) {
+            Player.declareTie();
+            console.log("tie");
           }
+          if (isWinner(Player)) {
+            Player.declareWinner();
+            console.log("WINNER");
+          }
+          switchTurn(Player1, Player2);
         //   updateGame();
         // }
         // if 3 in a row declare winner
@@ -94,6 +52,13 @@ const gameBoard = (() => {
     });
   }
 
+  const switchTurn = (Player1, Player2) => {
+    if (Player1.isTurn() ? Player1.finishedTurn() : Player1.startTurn());
+    if (Player2.isTurn() ? Player2.finishedTurn() : Player2.startTurn());
+    //console.log(Player1.isTurn());
+  }
+
+  // Check if there is a winner
   const isWinner = (Player) => {
     let winConditions = { 'row_1' : 0, 'row_2' : 0, 'row_3' : 0,
                           'column_1' : 0, 'column_2': 0, 'column_3': 0,
@@ -141,12 +106,28 @@ const gameBoard = (() => {
     }
     
     for (i = 0; i < Object.keys(winConditions).length; i++) {
-      console.log(Object.values(winConditions)[i]);
+      //console.log(Object.values(winConditions)[i]);
       if (Object.values(winConditions)[i] == 3) {
         return true;
       }
     }
     return false;
+  }
+
+  const isBoardFull = () => {
+    let total = 0;
+    for (i = 0; i < 9; i++) {
+      switch (Object.values(board)[i]) {
+        case 'X': total++; break;
+        case 'O': total++; break
+      }
+    }
+    return total == 9 ? true : false;
+  }
+
+  // Check if there is a tie
+  const isTie = () => {
+    return isBoardFull();
   }
 
   // Checks if a square is a valid move
@@ -157,42 +138,41 @@ const gameBoard = (() => {
   // Places a move on the board and stores it
   const placeMove = (square, Player) => {
     board[square.dataset.key] = Player.getSide();
+    //console.log(board[square.dataset.key]);
     return square.textContent = Player.getSide();   
   }
 
-  return {boardListen}
+  return {boardListen, isBoardFull}
 })();
 
 const game = (() => {
   const initGame = () => {
-    const player1 = Player('X')
+    const player1 = Player('X');
     const player2 = Player('O');
-    const round = gameBoard.boardListen(player1);   
+    player1.startTurn();
+    const round = gameBoard.boardListen(player1, player2);
+    
   };
 
-  const newBoard = (board) => {
+  const resetEvent = () => {
     //Event listeners on new game button
     const resetButton = document.querySelector('#new_game');
     resetButton.addEventListener('click', function() {
-      board.newBoard();
+      resetBoard();
     });
   }
 
-  //occurs when click new game button
-  //make Players
-  //Player 1 choose 'X' or 'O'
-  //make gameBoard
-  //Player 1 turn
-  //Players take turn till winner declared
-  return {initGame}
+  // Clear board and initialize
+  const resetBoard = () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(sq => {
+      sq.textContent = '';
+    });
+  }
+
+  return {initGame, resetEvent}
 })();
  
 
-
-//const player1 = Player('X');
-//console.log(player1.isTurn());
-//player1.startTurn();
-//console.log(player1.isTurn());
-//const round = gameBoard.boardListen(player1);
-
-const round = game.initGame();
+const newGame = game.initGame();
+const resetListener = game.resetEvent(newGame);
